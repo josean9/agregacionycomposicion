938e57acf88dd02dcae458b36f490ef8e8d03dc3
jose antonio <jsancesc@gmail.com>
2023-04-06 18:43:11 +0200

    __del__

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -17,10 +17,6 @@ del(yang)
 print("?") 
 >>> ? 
 Yang destruido"""
-class Yin: pass 
-class Yang: 
-    def __del__(self): 
-        print("Yang destruido") 
 class soldado():
     def __init__(self, nombre):
         self.nombre = nombre
@@ -28,15 +24,28 @@ class soldado():
         return self.nombre
     def __del__(self):
         print("Se ha destruido al soldado {}".format(self.nombre))
-yin = Yin() 
-yang = Yang() 
- 
 soldado1 = soldado("Martin")
 print(soldado1.__del__())
 soldado2 = soldado("Salim")
+for i in range(10):
+    print(i)
 print(soldado1)
+class Yin: pass 
+class Yang: 
+    def __del__(self): 
+        print("Yang destruido") 
+
+yin = Yin() 
+yang = Yang()
+yin.yang = yang 
+print(yang) 
+print(yang is yin.yang) 
+del(yang) 
+print("?")
 
 
-print("""yang es destruido ya que el metodo __del__ de la clase Yang es activado 
-tras crear el objeto yang de la clase Yang
+
+print("""yang es destruido despues de la interrogacion ya que el metodo __del__ de la clase Yang es activado 
+tras crear el objeto yang de la clase Yang obligatoriamente aunque no sea llamado, pero se activa una vex el codigo haya terminado, al igual
+que el __del__ de la clase soldado, que si lo llamamos, se activa al principio, sino al fianl del codigo
  """)
